2e1a1eb7df43097d67c26963666a72a4be951ea8
Romain Endelin <romain@endelin.fr>
2016-09-01 02:05:52 +0200

    Refactor word count

--- word-count/src/main/scala/Phrase.scala
+++ word-count/src/main/scala/Phrase.scala
@@ -2,9 +2,8 @@ import scala.collection.mutable.Map
 
 class Phrase(sentence: String) {
   val wordCount = sentence
-    .map((c: Char) => c.toLower)
-    .replaceAll("""[\p{Punct}&&[^,']]""", "") // removing all punctuation except comma and apostrophe
-    .split("[\\s,]+") // splitting by multiple whitespace, or by comma
+    .toLowerCase
+    .split("[^\\w']+") // splitting by anything but characters and apostrophes
     .foldLeft(Map[String, Int]()) {
       (m: Map[String, Int], str: String) => {
         m(str) = m.get(str).getOrElse(0) + 1 // find the string, increase its occurence (default: 0)
